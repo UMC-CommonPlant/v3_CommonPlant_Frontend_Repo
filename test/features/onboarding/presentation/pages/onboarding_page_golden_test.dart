874dfb3bca34a1d1ab0d198dcb5155d2ee51dbf5
김영우 <rladuddn3924@gmail.com>
2026-08-14 22:57:19 +0900

    Test: golden 실행 환경 고정 #199

--- test/features/onboarding/presentation/pages/onboarding_page_golden_test.dart
+++ test/features/onboarding/presentation/pages/onboarding_page_golden_test.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:commonplant_frontend/features/onboarding/presentation/pages/onboarding_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../helpers/golden_test.dart';
+import '../../../../helpers/golden_test_helper.dart';
 
 void main() {
   testWidgets('온보딩 화면은 Reference viewport 디자인을 유지한다', (tester) async {
@@ -11,5 +13,5 @@ void main() {
       find.byKey(goldenTestSurfaceKey),
       matchesGoldenFile('goldens/onboarding_page_375x812.png'),
     );
-  });
+  }, skip: !Platform.isLinux);
 }
